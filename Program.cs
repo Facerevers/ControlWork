57fc15b90102d8cd1c34d72513a09a9599f90a4a
crazle <crazle@mail.ru>
2023-08-28 17:02:56 +0300

    Написали код программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetNewMas(string[] mas){
+    int sizenewmas = 0;
+    foreach (string t in mas){
+        if (t.Length <= 3) sizenewmas++;
+    }
+    string[] newmas = new string[sizenewmas];
+    int i = 0;
+    foreach (string t in mas){
+        if (t.Length <= 3){
+            newmas[i] = t;
+            i++;
+        }
+    }
+    return newmas;
+}
+
+void ShowMas(string[] mas){
+    Console.Write("[");
+    for (int i = 0; i < mas.Length-1; i++){
+        Console.Write($"\"{mas[i]}\", ");
+    }
+    Console.Write($"\"{mas[mas.Length-1]}\"]");
+}
+
+string[] GetStrings(int size){
+    string[] mas = new string[size];
+    string txt = "";
+    for (int i = 0; i < size; i++){
+        Console.WriteLine("Введите строку: ");
+        mas[i] = txt + Console.ReadLine();
+    }
+    return mas;
+}
+
+int GetInput(string text){
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = GetInput("Введите количество строк: ");
+string[] mas = GetStrings(size);
+string[] newmas = GetNewMas(mas);
+ShowMas(mas);
+Console.Write(" -> ");
+ShowMas(newmas);
